51d8fcf20c2a03105001a4c311f68da8e2ca0f89
Evoxus <michael@michaelhploughman.com>
2019-12-10 22:50:03 -0700

    merge

--- src/general-components/card.test.js
+++ src/general-components/card.test.js
@@ -1,22 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Card";
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer'
+import Card from './Card';
 
-// smoke testing//
-describe("<Card/>", () => {
-  it("Card mounted succesful", () => {
-    const div = document.createElement("div");
+describe('<Card />', () => {
+  it('Mounts without error', () => {
+    const div = document.createElement('div');
     const dummyData = { id: "a", title: "First card", content: "lorem ipsum" };
-
-    ReactDOM.render(<Card data={dummyData} />, div);
+    ReactDOM.render(<Card data={dummyData}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  // snap shot testing//
-  it("matches snap shot", () => {
+  it('Matches snapshot', () => {
     const dummyData = { id: "a", title: "First card", content: "lorem ipsum" };
-    const tree = renderer.create(<Card data={dummyData} />).toJSON();
-
+    const tree = renderer.create(<Card data={dummyData}/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
