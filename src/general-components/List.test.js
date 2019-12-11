675615638d998dd5878c99eb4fb736aa108153f4
Evoxus <michael@michaelhploughman.com>
2019-12-10 22:47:06 -0700

    List test updated

--- src/general-components/List.test.js
+++ src/general-components/List.test.js
@@ -2,25 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import renderer from 'react-test-renderer'
 import List from './List';
+import STORE from '../store'
 
 describe('<List />', () => {
   it('Mounts without error', () => {
     const div = document.createElement('div');
-    const dummyData = {
-      id: "3",
-      header: "Third list",
-      cardIds: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
-    };
+    const dummyData = STORE.lists[0];
     ReactDOM.render(<List data={dummyData}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   it('Matches snapshot', () => {
-    const dummyData = {
-      id: "3",
-      header: "Third list",
-      cardIds: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]
-    };
-    const tree = renderer.create(<List data={dummyData}/>).toJSON();
+    const data = STORE.lists[0];
+    const tree = renderer.create(<List data={data}/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
